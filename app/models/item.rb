e1404512dcbabe5e47503e8b980e88252c8bc92d
Diethart <diethart19@gmail.com>
2018-09-11 17:14:11 +0300

    english comment

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
-# Класс, представляющий собой элементы чеклиста.
+# Class representing checklist elements
 class Item < ApplicationRecord
   validates :task, presence: true, length: { maximum: 100 }
 end
